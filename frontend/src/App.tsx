4699a0d2599be83aa2aaa2b00f42f25fb82fc2c0
Tiago Pereira <tiago.pereira@gmail.com>
2020-09-19 17:59:32 -0400

    Criando pagina Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <div>
     <Header />
+    <Home />
+
+    </div>
+
 
   );
 }
